d7ea1273f761304fbac3085bd4ad4351fe112e95
Tyler Whitehurst <twwhitehurst@gmail.com>
2020-10-09 06:47:12 -0400

    initial commit

---
+++ src/main/java/com/wbs/sql/AbstractDataModule.java
@@ -0,0 +1,32 @@
+package com.wbs.sql;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlite3.SQLitePlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
+import ru.vyarus.guicey.jdbi3.JdbiBundle;
+
+public abstract class AbstractDataModule<DAO> extends AbstractModule {
+    protected abstract String getNamespace();
+    protected abstract Class<DAO> getClazz();
+
+    @Override
+    protected void configure() {
+        GuiceBundle.builder()
+                .bundles(JdbiBundle.<JdbiAppConfiguration>forDatabase((conf, env) -> conf.getDatabase()));
+    }
+
+    @Provides
+    DAO provideDao() {
+        Jdbi dbi = locateDbi();
+        return dbi.open().attach(getClazz());
+    }
+
+    Jdbi locateDbi() {
+        return Jdbi.create(getNamespace())
+                .installPlugin(new SQLitePlugin())
+                .installPlugin(new SqlObjectPlugin());
+    }
+}
